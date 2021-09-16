c5807ffb8e0e90a28a2a72c5fb566f48518e235d
krishantha.dharmasiri@outlook.com <krishantha.dharmasiri@outlook.com>
2021-09-16 18:49:01 +0530

    Event handling in React

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import './App.css';
 
 import {Welcome} from  './components/welcome';
 import { Clock } from './components/clock';
+import { Toggle } from './components/Toggle';
 
 
 function App() {
+
+  let userData = {
+    name : "Saraha",
+    country : "Australia",
+    city:"Melbourne"
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         
-        <Welcome name="Sarah" />
+        <Welcome user={userData} />
         
         <Clock />
         
+        <Toggle></Toggle>
       </header>
     </div>
   );
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 export class Welcome extends React.Component{
     render(){
-        return <p>How are you, {this.props.name}</p>
+        return <p>How are you, {this.props.user.name} from {this.props.user.country} 
+         who lives in {this.props.user.city}</p>
     }
 }
 
